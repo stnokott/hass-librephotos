3f95de2ac8224b0813f3215aa394fd55cc96fe72
stnokott <nftrash@web.de>
2021-12-01 00:43:38 +0100

    remove redundant f-string

--- custom_components/librephotos/const/const.py
+++ custom_components/librephotos/const/const.py
@@ -7,7 +7,7 @@ KEY_ATTRS = "attributes"
 KEY_ATTR_WORKERS = "workers"
 
 QUERY_ACCESS_TOKEN = "api/auth/token/obtain/"
-QUERY_WORKERS = f"api/jobs/"
+QUERY_WORKERS = "api/jobs/"
 
 MAX_WORKERS_COUNT = 3
 STRPTIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
